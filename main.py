eb046a5320316dd583aa92f2ca407d7b5f9e2beb
vivas <jvivasb20@gmai.com>
2023-08-05 15:19:39 -0400

    ADD - banner settings

--- main.py
+++ main.py
@@ -9,19 +9,28 @@ from bing_image_downloader import downloader
 
 DATASET_DIR = f'{os.getcwd()}/dataset'
 
+TOLERANCE = 150
+
 IMAGE_FORMATS = [
     'png',
     'jpg',
     'jpeg'
 ]
 
+IMAGE_TYPES = [
+    'banners',
+    'covers'
+]
+
 BANNER_RATIOS = [
+    '2:1',
+    '5:3',
     '16:9',
-    '1920:883'
     '19:10',
-    '853:480',
+    '150:89',
+    '300:157'
     '320:183',
-    '150:89'
+    '1920:883'
 ]
 
 COVER_RATIOS = [
@@ -33,8 +42,6 @@ COVER_RATIOS = [
 
 ]
 
-TOLERANCE = 150
-
 
 def validate_dir(dirname):
     if os.path.exists(dirname):
@@ -45,27 +52,39 @@ def validate_dir(dirname):
         exit(1)
 
 
-def download_images(media_type, query_string, limit):
-    output_dir = f'{DATASET_DIR}/{query_string}'
+def download_images(media_type, query_string, dataset_limit):
+    # Here we define limit based on the dataset_limit parameter
+    if (dataset_limit == 'Small'):
+        limit = 33
+    elif (dataset_limit == 'Medium'):
+        limit = 67
+    elif (dataset_limit == 'Large'):
+        limit = 100
 
-    downloader.download(f'{query_string} {media_type}', limit=limit,  output_dir='dataset',
-                        adult_filter_off=True, force_replace=False, timeout=60, verbose=True)
-    validate_dir(output_dir)
-    process_images(output_dir, limit)
+    for img_type in IMAGE_TYPES:
+        query = f'{query_string} {media_type} {img_type}'
+        output_dir = f'{DATASET_DIR}/{query}'
+        downloader.download(query, limit=limit)
+        validate_dir(output_dir)
+        process_images(query, output_dir, limit, img_type)
 
 
 def pass_tolerance(img_width, img_height, dimension_width, dimension_height):
     return (abs(img_width - int(dimension_width)) <= TOLERANCE and abs(img_height - int(dimension_height)) <= TOLERANCE)
 
 
-def calculate_aspect(width: int, height: int) -> str:
+def calculate_ratio(width: int, height: int) -> str:
     r = math.gcd(width, height)
     x = int(width / r)
     y = int(height / r)
     return f"{x}:{y}"
 
 
-def process_images(output_dir, count):
+def process_images(query, output_dir, count, img_type):
+
+    print(f'Processing {img_type}...')
+    valid_img_num = 0
+    valid_img_name = query.lower().replace(' ', '_')
 
     for x in range(1, count + 1):
 
@@ -84,48 +103,53 @@ def process_images(output_dir, count):
         img_height = img.shape[0]
         img_width = img.shape[1]
 
-        print(calculate_aspect(img_width, img_height))
+        print(calculate_ratio(img_width, img_height))
 
-        # TODO: set a tolerance for the real image size and the banner/cover size
-        # TODO: check if the image is a banner or a cover
-        # TODO: if the image doesn't pass the check then delete it
-        # TODO: then based on the reaming images, iterate again an search more images
-
-        """ for dimension in BANNER_DIMENSIONS:
-            dimension_width, dimension_height = dimension.split('x')
-            if (pass_tolerance(img_width, img_height, dimension_width, dimension_height)):
-                print(f'Image {current_image} is a banner')
-                break
-         """
-
-        """ for dimension in COVER_DIMENSIONS:
-            dimension_width, dimension_height = dimension.split('x')
-            if (pass_tolerance(img_width, img_height, dimension_width, dimension_height)):
-                print(f'Image {current_image} is a cover')
-                break
-
-        print(f'Image {current_image} is not a banner or a cover') """
+        if img_type == 'banners':
+            valid_img = False
+            for ratio in BANNER_RATIOS:
+                if (ratio == calculate_ratio(img_width, img_height)):
+                    print('Banner found!')
+                    valid_img = True
+                    valid_img_num += 1
+                    os.rename(
+                        current_image, f'{output_dir}/{valid_img_name}_{valid_img_num}.{img_format}')
+                    continue
+            if (not valid_img):
+                os.remove(current_image)
 
 
 if __name__ == '__main__':
     try:
 
-        questions = [
+        media_type_questions = [
             inquirer.List('media_type',
                           message="Select the media type",
                           choices=['Movie', 'Serie'],
                           ),
         ]
-        answers = inquirer.prompt(questions)
-        media_type = answers['media_type']
+        media_type_answers = inquirer.prompt(media_type_questions)
+        media_type = media_type_answers['media_type']
+
+        # TODO: Set dataset limit parameters
+
         query_string = input(f'Enter the {media_type} name: ')
-        limit = int(input("Enter the number of images to download: "))
-        download_images(media_type, query_string, limit)
+
+        dataset_limit_questions = [
+            inquirer.List('dataset_limit',
+                          message="Select the dataset of images to download",
+                          choices=['Small', 'Medium', 'Large'],)
+        ]
+
+        dataset_limit_answers = inquirer.prompt(dataset_limit_questions)
+        dataset_limit = dataset_limit_answers['dataset_limit']
+
+        download_images(media_type, query_string, dataset_limit)
 
     except Exception as e:
         if (e.__class__.__name__ == 'ValueError'):
             print(
                 "There was an error with the limit value. Please try again using a valid limit.")
         else:
-            print("Please try again using a valid query string and limit.")
+            print(e)
         exit(1)
